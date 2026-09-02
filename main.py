c2f6449bb39f02b08711bd82e2c527a915402638
LimDongXian <limdongx@gmail.com>
2026-09-02 16:46:50 +0800

    Simple keylogger feature

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import asyncio
 import pygame
 import keyboard
 
 from constant import *
 
 def main():
-    pass
+    print("Welcome to Keyboard Gun!")
+    while keyboard.is_pressed('esc') == False:
+        event = keyboard.read_event()
+        if event.event_type == keyboard.KEY_DOWN:
+            print(f"You pressed: {event.name}")
+    print("Exiting the game. Goodbye!")
 
+main()
